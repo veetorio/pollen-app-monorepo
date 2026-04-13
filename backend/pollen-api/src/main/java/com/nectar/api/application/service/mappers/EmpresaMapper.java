e2c74ebd1c7a21b5e971b44be125060f325d27d9
veetorio <ettorevitorio98@gmail.com>
2026-04-12 22:09:56 -0300

    news

--- backend/pollen-api/src/main/java/com/nectar/api/application/service/mappers/EmpresaMapper.java
+++ backend/pollen-api/src/main/java/com/nectar/api/application/service/mappers/EmpresaMapper.java
@@ -2,7 +2,6 @@ package com.nectar.api.application.service.mappers;
 
 import com.nectar.api.controller.in.EmpresaDtoIn;
 import com.nectar.api.controller.out.empresarial.EmpresaOutput;
-import com.nectar.api.controller.out.workspace.AnotacaoOutput;
 import com.nectar.api.domain.models.empresarial.Empresa;
 
 import org.mapstruct.Mapper;
